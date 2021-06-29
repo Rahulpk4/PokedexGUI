5f3ff0956d775c78c0d70ef68c53ba8a9077bf8c
Rahul <rpkumar004@gmail.com>
2021-06-29 15:19:15 +0530

    Entry Box and Buttons

--- main.py
+++ main.py
@@ -12,4 +12,16 @@ User.place(x=190, y=170)
 Pass = Label(win, text="PASSWORD: ", fg="red", font=('Arial', 10))
 Pass.place(x=190, y=220)
 
+ent1 = Entry(win, width=30, bd=3)
+ent1.place(x=300, y=170)
+
+ent2 = Entry(win, show='*', width=30, bd=3)
+ent2.place(x=300, y=220)
+
+btn1 = Button(win, text="Submit", bg='#C70039', fg='white', font=('Arial', 10), bd=3, width=17)
+btn1.place(x=190, y=290)
+
+btn2 = Button(win, text="Cancel", bg='#C70039', fg='white', font=('Arial', 10), bd=3, width=17)
+btn2.place(x=370, y=290)
+
 win.mainloop()
