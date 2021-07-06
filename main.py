08b1d500dca9ffe83c29fd70f4039b90587cbe9d
Rahul <rpkumar004@gmail.com>
2021-07-06 22:18:10 +0530

    playing around

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import tkinter
 import pypokedex
+import numpy as np
+
+p = pypokedex.get(name="swampert")
+
+print (p.moves)
+
+for item in range(0, len(p.moves['omega-ruby'])):
+    print (p.moves['omega-ruby'][item])
+
+
+print(np.sum(np.array(p.base_stats)))
 
